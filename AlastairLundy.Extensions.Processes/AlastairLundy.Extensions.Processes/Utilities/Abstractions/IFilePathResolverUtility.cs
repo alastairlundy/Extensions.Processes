40619c5ad89ef8984fcb94b0abe7f0418cf3a01b
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-02-23 16:15:19 +0000

    create replacement for FilePathResolver

---
+++ AlastairLundy.Extensions.Processes/AlastairLundy.Extensions.Processes/Utilities/Abstractions/IFilePathResolverUtility.cs
@@ -0,0 +1,20 @@
+﻿/*
+    AlastairLundy.Extensions.Processes  
+    Copyright (C) 2024-2025  Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+namespace AlastairLundy.Extensions.Processes.Utilities.Abstractions;
+
+public interface IFilePathResolverUtility
+{
+    /// <summary>
+    /// Resolves the file path if the file path is a PATH environment variable.
+    /// </summary>
+    /// <param name="inputFilePath">The input file path to resolve.</param>
+    /// <param name="resolvedFilePath">The resolved file path if the file path is in the PATH environment variable; the original input file path otherwise.</param>
+    void ResolveFilePath(string inputFilePath, out string resolvedFilePath);
+}
---
+++ AlastairLundy.Extensions.Processes/AlastairLundy.Extensions.Processes/Utilities/FilePathResolverUtility.cs
@@ -0,0 +1,36 @@
+﻿/*
+    AlastairLundy.Extensions.Processes  
+    Copyright (C) 2024-2025  Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+using System.IO;
+using System.Linq;
+
+using AlastairLundy.Extensions.Processes.Utilities.Abstractions;
+
+namespace AlastairLundy.Extensions.Processes.Utilities;
+
+public class FilePathResolverUtility : IFilePathResolverUtility
+{
+    public void ResolveFilePath(string inputFilePath, out string resolvedFilePath)
+    {
+#if NET6_0_OR_GREATER || NETSTANDARD2_1
+        if (Path.IsPathFullyQualified(inputFilePath) == true)
+        {
+            resolvedFilePath = inputFilePath;
+            return;
+        }
+#endif
+        
+        char[] tempPath = inputFilePath.Where(x => Path.GetInvalidPathChars().Contains(x) == false && Path.GetInvalidFileNameChars().Contains(x) == false)
+            .ToArray();
+        
+        string newPath = string.Join(string.Empty, tempPath);
+        
+        resolvedFilePath = Path.GetFullPath(newPath);
+    }
+}
