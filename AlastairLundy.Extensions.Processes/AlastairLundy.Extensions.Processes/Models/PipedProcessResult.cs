941ed0c427fd49f8b4076b9c6b055218d565646e
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-04 13:51:10 +0000

    Create PipedProcessResult.cs

---
+++ AlastairLundy.Extensions.Processes/AlastairLundy.Extensions.Processes/Models/PipedProcessResult.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO.Pipelines;
+
+namespace AlastairLundy.Extensions.Processes;
+
+public class PipedProcessResult : ProcessResult
+{
+    public PipedProcessResult(string executableFilePath,
+        int exitCode,
+        DateTime startTime,
+        DateTime exitTime, Pipe standardOutput,
+        Pipe standardError) : base(executableFilePath,
+        exitCode,
+        startTime,
+        exitTime)
+    {
+        StandardOutput = standardOutput;
+        StandardError = standardError;
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public Pipe StandardOutput { get; init; }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    public Pipe StandardError { get; init; }
+}
