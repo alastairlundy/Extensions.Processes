6858d85671d4f3ab6a0fbe15556a32077a71d317
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-16 14:49:09 +0000

    add PipedProcessResult support to IProcessRunner

--- src/AlastairLundy.Extensions.Processes.Abstractions/IProcessRunner.cs
+++ src/AlastairLundy.Extensions.Processes.Abstractions/IProcessRunner.cs
@@ -31,7 +31,30 @@ namespace AlastairLundy.Extensions.Processes.Abstractions
          Task<ProcessResult> ExecuteProcessAsync(Process process, ProcessResultValidation processResultValidation,
             ProcessResourcePolicy? processResourcePolicy = null,
             CancellationToken cancellationToken = default);
-
+        
+        /// <summary>
+        /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
+        /// </summary>
+        /// <param name="process">The process to be run.</param>
+        /// <param name="processConfiguration">The process configuration to use when running the Process.</param>
+        /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+        /// <returns>The Piped Process Results from running the process.</returns>
+        Task<PipedProcessResult> ExecutePipedProcessAsync(Process process, 
+            ProcessConfiguration processConfiguration,
+            CancellationToken cancellationToken = default);
+        
+        /// <summary>
+        /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
+        /// </summary>
+        /// <param name="process">The process to be run.</param>
+        /// <param name="processResultValidation">The process result validation to be used.</param>
+        /// <param name="processResourcePolicy">The process resource policy to be set if it is not null.</param>
+        /// <param name="cancellationToken">A token to cancel the operation if required.</param>
+        /// <returns>The Piped Process Results from running the process.</returns>
+        Task<PipedProcessResult> ExecutePipedProcessAsync(Process process, ProcessResultValidation processResultValidation,
+            ProcessResourcePolicy? processResourcePolicy = null,
+            CancellationToken cancellationToken = default);
+        
         /// <summary>
         /// Runs the process asynchronously, waits for exit, and safely disposes of the Process before returning.
         /// </summary>
