3f43848be2437295f6c3d19068fd4d96c6089396
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-16 14:21:29 +0000

    remove access modifiers from IProcessFactory

--- src/AlastairLundy.Extensions.Processes.Abstractions/IProcessFactory.cs
+++ src/AlastairLundy.Extensions.Processes.Abstractions/IProcessFactory.cs
@@ -23,7 +23,7 @@ namespace AlastairLundy.Extensions.Processes.Abstractions
         /// </summary>
         /// <param name="startInfo">The start information to use for the Process.</param>
         /// <returns>The newly created Process.</returns>
-        public Process From(ProcessStartInfo startInfo);
+        Process From(ProcessStartInfo startInfo);
     
         /// <summary>
         /// Creates a process from the specified start info and UserCredential.
@@ -31,21 +31,21 @@ namespace AlastairLundy.Extensions.Processes.Abstractions
         /// <param name="startInfo">The start information to use for the Process.</param>
         /// <param name="credential">The credential to use when creating the Process.</param>
         /// <returns>The newly created Process.</returns>
-        public Process From(ProcessStartInfo startInfo, UserCredential credential);
+        Process From(ProcessStartInfo startInfo, UserCredential credential);
     
         /// <summary>
         /// Creates a process from the specified process configuration.
         /// </summary>
         /// <param name="configuration">The configuration information to use to configure the Process.</param>
         /// <returns>The newly created Process with the configuration.</returns>
-        public Process From(ProcessConfiguration configuration);
+        Process From(ProcessConfiguration configuration);
     
         /// <summary>
         /// Creates and starts a new Process with the specified Process Start Info.
         /// </summary>
         /// <param name="startInfo">The start info to use when creating and starting the new Process.</param>
         /// <returns>The newly created and started Process with the start info.</returns>
-        public Process StartNew(ProcessStartInfo startInfo);
+        Process StartNew(ProcessStartInfo startInfo);
     
         /// <summary>
         /// Creates and starts a new Process with the specified Process Start Info and credential.
@@ -53,7 +53,7 @@ namespace AlastairLundy.Extensions.Processes.Abstractions
         /// <param name="startInfo">The start info to use when creating and starting the new Process.</param>
         /// <param name="credential">The credential to use when creating and starting the Process.</param>
         /// <returns>The newly created and started Process with the start info and credential.</returns>
-        public Process StartNew(ProcessStartInfo startInfo, UserCredential credential);
+         Process StartNew(ProcessStartInfo startInfo, UserCredential credential);
     
         /// <summary>
         /// Creates and starts a new Process with the specified Process Start Info and Process Resource policy.
@@ -61,7 +61,7 @@ namespace AlastairLundy.Extensions.Processes.Abstractions
         /// <param name="startInfo">The start info to use when creating and starting the new Process.</param>
         /// <param name="resourcePolicy">The process resource policy to use when creating and starting the new Process.</param>
         /// <returns>The newly created and started Process with the start info and Process Resource Policy.</returns>
-        public Process StartNew(ProcessStartInfo startInfo, ProcessResourcePolicy resourcePolicy);
+         Process StartNew(ProcessStartInfo startInfo, ProcessResourcePolicy resourcePolicy);
     
         /// <summary>
         /// Creates and starts a new Process with the specified Process Start Info, credential, and Process Resource policy.
@@ -70,14 +70,14 @@ namespace AlastairLundy.Extensions.Processes.Abstractions
         /// <param name="resourcePolicy">The process resource policy to use when creating and starting the new Process.</param>
         /// <param name="credential">The credential to use when creating and starting the Process.</param>
         /// <returns>The newly created and started Process with the start info and Process Resource Policy.</returns>
-        public Process StartNew(ProcessStartInfo startInfo, ProcessResourcePolicy resourcePolicy, UserCredential credential);
+         Process StartNew(ProcessStartInfo startInfo, ProcessResourcePolicy resourcePolicy, UserCredential credential);
     
         /// <summary>
         /// Creates and starts a new Process with the specified configuration.
         /// </summary>
         /// <param name="configuration">The configuration to use when creating and starting the process.</param>
         /// <returns>The newly created and started Process with the specified configuration.</returns>
-        public Process StartNew(ProcessConfiguration configuration);
+         Process StartNew(ProcessConfiguration configuration);
 
         /// <summary>
         /// Creates a Task that returns a ProcessResult when the specified process exits.
@@ -85,7 +85,7 @@ namespace AlastairLundy.Extensions.Processes.Abstractions
         /// <param name="process">The process to continue and wait for exit.</param>
         /// <param name="cancellationToken">The cancellation token to use in case cancellation is requested.</param>
         /// <returns>The task and processResult that are returned upon completion of the task.</returns>
-        public Task<ProcessResult> ContinueWhenExitAsync(Process process, CancellationToken cancellationToken = default);
+         Task<ProcessResult> ContinueWhenExitAsync(Process process, CancellationToken cancellationToken = default);
     
         /// <summary>
         /// Creates a Task that returns a ProcessResult when the specified process exits.
@@ -94,7 +94,7 @@ namespace AlastairLundy.Extensions.Processes.Abstractions
         /// <param name="resultValidation">Whether to perform Result validation on the process' exit code.</param>
         /// <param name="cancellationToken">The cancellation token to use in case cancellation is requested.</param>
         /// <returns>The task and ProcessResult that are returned upon completion of the task.</returns>
-        public Task<ProcessResult> ContinueWhenExitAsync(Process process, ProcessResultValidation resultValidation, CancellationToken cancellationToken = default);
+         Task<ProcessResult> ContinueWhenExitAsync(Process process, ProcessResultValidation resultValidation, CancellationToken cancellationToken = default);
     
         /// <summary>
         /// Creates a Task that returns a BufferedProcessResult when the specified process exits.
@@ -102,7 +102,7 @@ namespace AlastairLundy.Extensions.Processes.Abstractions
         /// <param name="process">The process to continue and wait for exit.</param>
         /// <param name="cancellationToken">The cancellation token to use in case cancellation is requested.</param>
         /// <returns>The task and BufferedProcessResult that are returned upon completion of the task.</returns>
-        public Task<BufferedProcessResult> ContinueWhenExitBufferedAsync(Process process, CancellationToken cancellationToken = default);
+         Task<BufferedProcessResult> ContinueWhenExitBufferedAsync(Process process, CancellationToken cancellationToken = default);
     
         /// <summary>
         /// Creates a Task that returns a BufferedProcessResult when the specified process exits.
@@ -111,6 +111,6 @@ namespace AlastairLundy.Extensions.Processes.Abstractions
         /// <param name="resultValidation">Whether to perform Result validation on the process' exit code.</param>
         /// <param name="cancellationToken">The cancellation token to use in case cancellation is requested.</param>
         /// <returns>The task and BufferedProcessResult that are returned upon completion of the task.</returns>
-        public Task<BufferedProcessResult> ContinueWhenExitBufferedAsync(Process process, ProcessResultValidation resultValidation, CancellationToken cancellationToken = default);
+         Task<BufferedProcessResult> ContinueWhenExitBufferedAsync(Process process, ProcessResultValidation resultValidation, CancellationToken cancellationToken = default);
     }
 }
