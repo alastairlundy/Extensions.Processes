d9747187565b42b81890ed953f8e18fe3c060a78
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-18 11:08:17 +0000

    update models in implementation class

--- src/AlastairLundy.Extensions.Processes/Models/ProcessConfiguration.cs
+++ src/AlastairLundy.Extensions.Processes/Models/ProcessConfiguration.cs
@@ -11,107 +11,131 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+
 // ReSharper disable UnusedAutoPropertyAccessor.Global
 // ReSharper disable ClassNeverInstantiated.Global
 
 namespace AlastairLundy.Extensions.Processes;
-
-/// <summary>
-/// A class to store Process configuration information.
-/// </summary>
 public class ProcessConfiguration
 {
         /// <summary>
-        /// Instantiates the Process Configuration class with a ProcessStartInfo and other optional parameters.
+        /// A class to store Process configuration information.
         /// </summary>
-        /// <param name="processStartInfo"></param>
-        /// <param name="environmentVariables">The environment variables to be set (if specified).</param>
-        /// <param name="credential">The credential to be used (if specified).</param>
-        /// <param name="resultValidation">Whether to perform Result Validation and exception throwing if the Command exits with an exit code other than 0.</param>
-        /// <param name="timeOutThreshold"></param>
-        /// <param name="standardInput">The standard input source to be used (if specified).</param>
-        /// <param name="standardOutput">The standard output destination to be used (if specified).</param>
-        /// <param name="standardError">The standard error destination to be used (if specified).</param>
-        /// <param name="processResourcePolicy">The process resource policy to be used (if specified).</param>
-        public ProcessConfiguration(ProcessStartInfo processStartInfo,
-                IReadOnlyDictionary<string, string>? environmentVariables = null,
-                Processes.Abstractions.UserCredential? credential = null,
-                Processes.Abstractions.ProcessResultValidation resultValidation = Processes.Abstractions.ProcessResultValidation.ExitCodeZero,
-                TimeSpan timeOutThreshold = default,
-                StreamWriter? standardInput = null,
-                StreamReader? standardOutput = null,
-                StreamReader? standardError = null,
-                Processes.Abstractions.ProcessResourcePolicy? processResourcePolicy = null)
         {
-                StartInfo = processStartInfo;
-                EnvironmentVariables = environmentVariables ?? new Dictionary<string, string>();
-                
-                Credential = credential ?? Processes.Abstractions.UserCredential.Null;
-            
-                ResourcePolicy = processResourcePolicy ?? Processes.Abstractions.ProcessResourcePolicy.Default;
-
-                ResultValidation = resultValidation;
-
-                StandardInput = standardInput ?? StreamWriter.Null;
-                StandardOutput = standardOutput ?? StreamReader.Null;
-                StandardError = standardError ?? StreamReader.Null;
-
-                TimeoutThreshold = timeOutThreshold == default ? TimeSpan.FromMinutes(30) : timeOutThreshold;
-
-                if (credential != null && credential.IsSupportedOnCurrentOS())
+                /// <summary>
+                /// Instantiates the Process Configuration class with a ProcessStartInfo and other optional parameters.
+                /// </summary>
+                /// <param name="processStartInfo"></param>
+                /// <param name="environmentVariables">The environment variables to be set (if specified).</param>
+                /// <param name="credential">The credential to be used (if specified).</param>
+                /// <param name="resultValidation">Whether to perform Result Validation and exception throwing if the Command exits with an exit code other than 0.</param>
+                /// <param name="timeOutThreshold"></param>
+                /// <param name="standardInput">The standard input source to be used (if specified).</param>
+                /// <param name="standardOutput">The standard output destination to be used (if specified).</param>
+                /// <param name="standardError">The standard error destination to be used (if specified).</param>
+                /// <param name="processResourcePolicy">The process resource policy to be used (if specified).</param>
+                public ProcessConfiguration(ProcessStartInfo processStartInfo,
+                        IReadOnlyDictionary<string, string>? environmentVariables = null,
+                        UserCredential? credential = null,
+                        ProcessResultValidation resultValidation = ProcessResultValidation.ExitCodeZero,
+                        TimeSpan timeOutThreshold = default,
+                        StreamWriter? standardInput = null,
+                        StreamReader? standardOutput = null,
+                        StreamReader? standardError = null,
+                        ProcessResourcePolicy? processResourcePolicy = null)
                 {
+                        StartInfo = processStartInfo;
+                        EnvironmentVariables = environmentVariables ?? new Dictionary<string, string>();
+                
+                        Credential = credential ?? UserCredential.Null;
+            
+                        ResourcePolicy = processResourcePolicy ?? ProcessResourcePolicy.Default;
+
+                        ResultValidation = resultValidation;
+
+                        StandardInput = standardInput ?? StreamWriter.Null;
+                        StandardOutput = standardOutput ?? StreamReader.Null;
+                        StandardError = standardError ?? StreamReader.Null;
+
+                        TimeoutThreshold = timeOutThreshold == default ? TimeSpan.FromMinutes(30) : timeOutThreshold;
+
+#if NET5_0_OR_GREATER
+                        if (credential != null && OperatingSystem.IsWindows())
+#else
+                                
+#endif
+                        {
 #pragma warning disable CA1416
-                        processStartInfo.AddUserCredential(credential);
+                                if (credential.UserName != null)
+                                {
+                                        processStartInfo.Domain = credential.Domain;
+                                }
+                                
+                                if (credential.Domain != null)
+                                {
+                                        processStartInfo.UserName = credential.UserName;
+                                }
+
+                                if (credential.Password != null)
+                                {
+                                        processStartInfo.Password = credential.Password;
+                                }
+
+                                if (credential.LoadUserProfile != null)
+                                { 
+                                        processStartInfo.LoadUserProfile = (bool)credential.LoadUserProfile;
+                                }
 #pragma warning restore CA1416
+                        }
                 }
+        
+                /// <summary>
+                /// The environment variables to be set.
+                /// </summary>
+                public IReadOnlyDictionary<string, string> EnvironmentVariables { get; protected set; }
+        
+                /// <summary>
+                /// The timespan after which a Process should no longer be allowed to continue waiting to exit.
+                /// </summary>
+                public TimeSpan TimeoutThreshold { get; private set; }
+        
+                /// <summary>
+                /// The Process start information to be used.
+                /// </summary>
+                public ProcessStartInfo StartInfo { get; private set; }
+        
+                /// <summary>
+                /// The credential to be used when executing the Command.
+                /// </summary>
+                public UserCredential? Credential { get; protected set; }
+
+                /// <summary>
+                /// The result validation to apply to the Command when it is executed.
+                /// </summary>
+                public ProcessResultValidation ResultValidation { get; protected set; }
+
+                /// <summary>
+                /// The Standard Input source to redirect Standard Input to if configured.
+                /// </summary>
+                /// <remarks>Using Shell Execution whilst also Redirecting Standard Input will throw an Exception. This is a known issue with the System Process class.</remarks>
+                /// <seealso href="https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.processstartinfo.redirectstandarderror" />
+                public StreamWriter? StandardInput { get; protected set; }
+
+                /// <summary>
+                /// The Standard Output target to redirect Standard Output to if configured.
+                /// </summary>
+                public StreamReader? StandardOutput { get; protected set; }
+
+                /// <summary>
+                /// The Standard Error target to redirect Standard Output to if configured.
+                /// </summary>
+                public StreamReader? StandardError { get; protected set; }
+
+                /// <summary>
+                /// The Process Resource Policy to be used for executing the Command.
+                /// </summary>
+                /// <remarks>Process Resource Policy objects enable configuring Processor Affinity and other resource settings to be applied to the Command if supported by the currently running operating system.
+                /// <para>Not all properties of a Process Resource Policy support all operating systems. Check before configuring a property.</para></remarks>
+                public ProcessResourcePolicy? ResourcePolicy { get; protected set; }
         }
-        
-        /// <summary>
-        /// The environment variables to be set.
-        /// </summary>
-        public IReadOnlyDictionary<string, string> EnvironmentVariables { get; protected set; }
-        
-        /// <summary>
-        /// The timespan after which a Process should no longer be allowed to continue waiting to exit.
-        /// </summary>
-        public TimeSpan TimeoutThreshold { get; private set; }
-        
-        /// <summary>
-        /// The Process start information to be used.
-        /// </summary>
-        public ProcessStartInfo StartInfo { get; private set; }
-        
-        /// <summary>
-        /// The credential to be used when executing the Command.
-        /// </summary>
-        public Processes.Abstractions.UserCredential? Credential { get; protected set; }
-
-        /// <summary>
-        /// The result validation to apply to the Command when it is executed.
-        /// </summary>
-        public Processes.Abstractions.ProcessResultValidation ResultValidation { get; protected set; }
-
-        /// <summary>
-        /// The Standard Input source to redirect Standard Input to if configured.
-        /// </summary>
-        /// <remarks>Using Shell Execution whilst also Redirecting Standard Input will throw an Exception. This is a known issue with the System Process class.</remarks>
-        /// <seealso href="https://learn.microsoft.com/en-us/dotnet/api/system.diagnostics.processstartinfo.redirectstandarderror" />
-        public StreamWriter? StandardInput { get; protected set; }
-
-        /// <summary>
-        /// The Standard Output target to redirect Standard Output to if configured.
-        /// </summary>
-        public StreamReader? StandardOutput { get; protected set; }
-
-        /// <summary>
-        /// The Standard Error target to redirect Standard Output to if configured.
-        /// </summary>
-        public StreamReader? StandardError { get; protected set; }
-
-        /// <summary>
-        /// The Process Resource Policy to be used for executing the Command.
-        /// </summary>
-        /// <remarks>Process Resource Policy objects enable configuring Processor Affinity and other resource settings to be applied to the Command if supported by the currently running operating system.
-        /// <para>Not all properties of a Process Resource Policy support all operating systems. Check before configuring a property.</para></remarks>
-        public Processes.Abstractions.ProcessResourcePolicy? ResourcePolicy { get; protected set; }
 }
--- src/AlastairLundy.Extensions.Processes/Models/ProcessResult.cs
+++ src/AlastairLundy.Extensions.Processes/Models/ProcessResult.cs
@@ -24,35 +24,50 @@ namespace AlastairLundy.Extensions.Processes
     /// <summary>
     /// A class that represents the results from an executed Process or Command.
     /// </summary>
-    public class ProcessResult(
-        string executableFilePath,
-        int exitCode,
-        DateTime startTime,
-        DateTime exitTime)
+    public class ProcessResult
     {
+        /// <summary>
+        /// Instantiates a ProcessResult with data about a Process' execution.
+        /// </summary>
+        /// <param name="executableFilePath">The file path of the file that was executed.</param>
+        /// <param name="exitCode">The process' exit code.</param>
+        /// <param name="startTime">The start time of the process.</param>
+        /// <param name="exitTime">The exit time of the process.</param>
+        public ProcessResult(string executableFilePath,
+            int exitCode,
+            DateTime startTime,
+            DateTime exitTime)
+        {
+            ExitCode = exitCode;
+            ExecutedFilePath = executableFilePath;
+            StartTime = startTime;
+            ExitTime = exitTime;
+        }
+
         /// <summary>
         /// Whether the Command successfully exited.
         /// </summary>
         public bool WasSuccessful => ExitCode == 0;
+        
         /// <summary>
         /// The exit code from the Command that was executed.
         /// </summary>
-        public int ExitCode { get; } = exitCode;
+        public int ExitCode { get; }
 
         /// <summary>
-        /// 
+        /// The file path of the file to be executed.
         /// </summary>
-        public string ExecutedFilePath { get; } = executableFilePath;
+        public string ExecutedFilePath { get; }
 
         /// <summary>
         /// The Date and Time that the Command's execution started.
         /// </summary>
-        public DateTime StartTime { get; } = startTime;
+        public DateTime StartTime { get; }
 
         /// <summary>
         /// The Date and Time that the Command's execution finished.
         /// </summary>
-        public DateTime ExitTime { get; } = exitTime;
+        public DateTime ExitTime { get; }
 
         /// <summary>
         /// How long the Command took to execute represented as a TimeSpan.
