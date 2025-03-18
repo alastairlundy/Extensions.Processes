2d23bc54a22a05d9579cd71ea8b0a05f8741f00f
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2025-03-18 10:59:04 +0000

    add missing xml doc comments

--- src/AlastairLundy.Extensions.Processes.Abstractions/IProcessRunner.cs
+++ src/AlastairLundy.Extensions.Processes.Abstractions/IProcessRunner.cs
@@ -1,4 +1,13 @@
-﻿using System.Diagnostics;
+﻿/*
+    AlastairLundy.Extensions.Processes.Abstractions
+    Copyright (C) 2024-2025  Alastair Lundy
+
+    This Source Code Form is subject to the terms of the Mozilla Public
+    License, v. 2.0. If a copy of the MPL was not distributed with this
+    file, You can obtain one at http://mozilla.org/MPL/2.0/.
+   */
+
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 
